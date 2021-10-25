d6f804a4c5a5c970dcfd9df8bd06ba2cf684f500
SOMYA SINGH <singhsomya121@gmail.com>
2021-10-25 23:32:09 +0530

    commit

--- pattern.java
+++ pattern.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main {
+public class pattern {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int N = in.nextInt();
